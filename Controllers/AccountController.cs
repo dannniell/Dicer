64b6d4f5c2cc96f3c6bf8adafcb873444bb1e5ea
daniel <danielalferia9@gmail.com>
2022-09-30 10:40:37 +0700

    add role to register user

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -11,12 +11,15 @@ namespace Dicer.Controllers
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
         public AccountController(UserManager<ApplicationUser> userManager,
-                                SignInManager<ApplicationUser> signInManager)
+                                SignInManager<ApplicationUser> signInManager,
+                                RoleManager<IdentityRole> roleManager)
         {
             this._userManager = userManager;
             this._signInManager = signInManager;
+            this._roleManager = roleManager;
         }
 
         [HttpGet]
@@ -25,18 +28,44 @@ namespace Dicer.Controllers
             return View();
         }
 
+
         [HttpPost]
-        public async Task<IActionResult> Register(Register model)
+        public async Task<IActionResult> Register(Register model, string submit)
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { Email = model.Email, UserName = model.Email };
+                var user = new ApplicationUser { Email = model.Email, UserName = model.Email, Name = model.Name };
                 var result = await _userManager.CreateAsync(user, model.Password);
 
-                if (result.Succeeded)
+                switch (submit)
                 {
-                    await _signInManager.SignInAsync(user, isPersistent: false);
-                    return RedirectToAction("index", "home");
+                    //add role client
+                    case "submitClient":
+                        {
+                            if (result.Succeeded)
+                            {
+                                var role = await _roleManager.FindByIdAsync("4e0544fd-c65f-4a81-bc93-eaaf6a510042");
+                                await _userManager.AddToRoleAsync(user, role.Name);
+                                await _signInManager.SignInAsync(user, isPersistent: false);
+                                // redirect to client home page
+                                return RedirectToAction("index", "home");
+                            }
+                            break;
+                        }
+
+                    //add role creator
+                    case "submitCreator":
+                        {
+                            if (result.Succeeded)
+                            {
+                                var role = await _roleManager.FindByIdAsync("8054b692-c227-4c5f-9837-40b2346fa8d3");
+                                await _userManager.AddToRoleAsync(user, role.Name);
+                                await _signInManager.SignInAsync(user, isPersistent: false);
+                                //redirect to creator home page
+                                return RedirectToAction("index", "home");
+                            }
+                            break;
+                        }
                 }
                 
                 foreach (var error in result.Errors)
@@ -47,7 +76,7 @@ namespace Dicer.Controllers
             /*var userData = await GetCurrentUserAsync();
             var userPekerjaan = userData.Pekerjaan;*/
             return View(model);
-            
+
         }
 
         private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
