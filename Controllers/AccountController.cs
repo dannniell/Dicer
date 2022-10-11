d20fa67fb18f5d1864ba55d7877bba6afbfee14f
daniel <danielalferia9@gmail.com>
2022-10-11 20:38:18 +0700

    add register creator

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -21,54 +21,61 @@ namespace Dicer.Controllers
             _roleManager = roleManager;
         }
 
+        #region Register Client
         [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
 
-
         [HttpPost]
-        public async Task<IActionResult> Register(Register model, string submit)
+        public async Task<IActionResult> Register(Register model)
         {
-            /*String client = "Client";
-            String creator = "Creator";
-            var roleResult1 = await _roleManager.CreateAsync(new IdentityRole(client));
-            var roleResult2 = await _roleManager.CreateAsync(new IdentityRole(creator));*/
             if (ModelState.IsValid)
             {
                 var user = new ApplicationUser { Email = model.Email, UserName = model.Email, Name = model.Name };
                 var result = await _userManager.CreateAsync(user, model.Password);
-
-                switch (submit)
+                if (result.Succeeded)
                 {
-                    //add role client
-                    case "submitClient":
-                        {
-                            if (result.Succeeded)
-                            {
-                                var role = await _roleManager.FindByIdAsync(Constants.Constants.roleIdClient);
-                                await _userManager.AddToRoleAsync(user, role.Name);
-                                await _signInManager.SignInAsync(user, isPersistent: false);
-                                // redirect to client home page
-                                return RedirectToAction("index", "home");
-                            }
-                            break;
-                        }
+                    var role = await _roleManager.FindByIdAsync(Constants.Constants.roleIdClient);
+                    await _userManager.AddToRoleAsync(user, role.Name);
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    // redirect to client home page
+                    return RedirectToAction("index", "home");
+                }
 
-                    //add role creator
-                    case "submitCreator":
-                        {
-                            if (result.Succeeded)
-                            {
-                                var role = await _roleManager.FindByIdAsync(Constants.Constants.roleIdCreator);
-                                await _userManager.AddToRoleAsync(user, role.Name);
-                                await _signInManager.SignInAsync(user, isPersistent: false);
-                                //redirect to creator home page
-                                return RedirectToAction("index", "home");
-                            }
-                            break;
-                        }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View(model);
+        }
+        #endregion Register Client
+
+        #region Register Creator
+        [HttpGet]
+        public IActionResult RegisterCreator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterCreator(RegisterCreator model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { Email = model.Email, UserName = model.Email, Name = model.Name, UserNameIg = model.UsernameIg, 
+                                                    Kota = model.Kota, Provinsi = model.Provinsi};
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    var role = await _roleManager.FindByIdAsync(Constants.Constants.roleIdCreator);
+                    await _userManager.AddToRoleAsync(user, role.Name);
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    //redirect to creator home page
+                    return RedirectToAction("index", "home");
                 }
                 
                 foreach (var error in result.Errors)
@@ -76,11 +83,9 @@ namespace Dicer.Controllers
                     ModelState.AddModelError(string.Empty, error.Description);
                 }
             }
-            /*var userData = await GetCurrentUserAsync();
-            var userPekerjaan = userData.Pekerjaan;*/
             return View(model);
-
         }
+        #endregion Register Creator
 
         [HttpPost]
         public async Task<IActionResult> Logout()
@@ -89,7 +94,60 @@ namespace Dicer.Controllers
             return RedirectToAction("Index", "Landing");
         }
 
-
         private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
+        /*[HttpPost]
+       public async Task<IActionResult> Register(Register model, string submit)
+       {
+           *//*String client = "Client";
+           String creator = "Creator";
+           var roleResult1 = await _roleManager.CreateAsync(new IdentityRole(client));
+           var roleResult2 = await _roleManager.CreateAsync(new IdentityRole(creator));*//*
+           if (ModelState.IsValid)
+           {
+               var user = new ApplicationUser { Email = model.Email, UserName = model.Email, Name = model.Name };
+               var result = await _userManager.CreateAsync(user, model.Password);
+
+               switch (submit)
+               {
+                   //add role client
+                   case "submitClient":
+                       {
+                           if (result.Succeeded)
+                           {
+                               var role = await _roleManager.FindByIdAsync(Constants.Constants.roleIdClient);
+                               await _userManager.AddToRoleAsync(user, role.Name);
+                               await _signInManager.SignInAsync(user, isPersistent: false);
+                               // redirect to client home page
+                               return RedirectToAction("index", "home");
+                           }
+                           break;
+                       }
+
+                   //add role creator
+                   case "submitCreator":
+                       {
+                           if (result.Succeeded)
+                           {
+                               var role = await _roleManager.FindByIdAsync(Constants.Constants.roleIdCreator);
+                               await _userManager.AddToRoleAsync(user, role.Name);
+                               await _signInManager.SignInAsync(user, isPersistent: false);
+                               //redirect to creator home page
+                               return RedirectToAction("index", "home");
+                           }
+                           break;
+                       }
+               }
+
+               foreach (var error in result.Errors)
+               {
+                   ModelState.AddModelError(string.Empty, error.Description);
+               }
+           }
+           *//*var userData = await GetCurrentUserAsync();
+           var userPekerjaan = userData.Pekerjaan;*//*
+           return View(model);
+
+       }*/
     }
 }
---
+++ Models/RegisterCreator.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dicer.Models
+{
+    public class RegisterCreator
+    {
+        [Required]
+        [StringLength(128, ErrorMessage ="Name max character is 128", MinimumLength = 1)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Username Instagram")]
+        public string UsernameIg { get; set; }
+
+        [Required]
+        [Display(Name = "Kota")]
+        public string Kota { get; set; }
+
+        [Required]
+        [Display(Name = "Provinsi")]
+        public string Provinsi { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
