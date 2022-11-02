b18b6fbf89aef9a6dd15da859a62c2f51f0ac708
billymoses <mosesbilly@gmail.com>
2022-11-02 22:33:36 +0700

    Add sort for home creator

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).on("change", "#sortCategory", function () {
+    if ($('#sortCategory').val() === 'Location') {
+        $("#sortContainer").addClass('mr-3');
+        $("#locationContainer").removeClass('d-none');
+        $("#genreContainer").addClass('d-none');
+        $("#genreContainer").val('').change();
+    }
+    else if ($('#sortCategory').val() === 'Type') {
+        $("#sortContainer").addClass('mr-3');
+        $("#genreContainer").removeClass('d-none');
+        $("#locationContainer").addClass('d-none');
+        $("#locationContainer").val('').change();
+
+    }
+    else if ($('#sortCategory').val() === 'All') {
+        $("#sortContainer").removeClass('mr-3');
+        $("#locationContainer").addClass('d-none');
+        $("#locationContainer").val('').change();
+        $("#genreContainer").addClass('d-none');
+        $("#genreContainer").val('').change();
+    }
+});
