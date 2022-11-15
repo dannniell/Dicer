7c53481b658ea80b7b0b87ac6747e46014a2bc88
daniel <danielalferia9@gmail.com>
2022-11-15 22:46:46 +0700

    Address feedback detail campaign insight

--- wwwroot/js/DetailClientCampaign.js
+++ wwwroot/js/DetailClientCampaign.js
@@ -1,7 +1,20 @@
 ﻿$(document).ready(function () {
     var InitProvinsi = document.getElementById("InitProvinsi").value;
     var InitKota = document.getElementById("InitKota").value;
+    var tag = document.getElementById("InitTypeTag").value;
 
+    if (tag == "IG Feed")
+    {
+        document.getElementById("typeTag").style.backgroundColor = "#EFA46B";
+    }
+    else if (tag == "IG Story") 
+    {
+        document.getElementById("typeTag").style.backgroundColor = "#5cd175";
+    }
+    else if (tag == "IG Reels")
+    {
+        document.getElementById("typeTag").style.backgroundColor = "#e37d7d";
+    }
     $("#provinsi").val(InitProvinsi);
     $("#kota").val(InitKota);
 });
