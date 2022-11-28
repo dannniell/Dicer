9963e147d698163dc543ec00a00232c65122638b
daniel <danielalferia9@gmail.com>
2022-11-28 20:42:24 +0700

    base MyCampaign

---
+++ Controllers/MyCampaign.cs
@@ -0,0 +1,60 @@
+﻿using Dicer.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Dicer.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Dicer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dicer.Controllers
+{
+    public class MyCampaign : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public MyCampaign(UserManager<ApplicationUser> userManager,
+                                ApplicationDbContext context)
+        {
+            this._userManager = userManager;
+            this._context = context;
+        }
+
+        #region Registered
+        [Authorize(Roles = Constants.Constants.roleNameClient)]
+        [HttpGet]
+        public async Task<IActionResult> Registered(int? pageNumber)
+        {
+            var user = await GetCurrentUserAsync();
+            var campaigns = from campaign in _context.Campaign
+                    from a in _context.ClientCampaign
+                    where a.UserId == user.Id && a.CampaignId == campaign.CampaignId 
+                        && campaign.IsDone == false
+                    select campaign;
+            
+            int pageSize = 6;
+            return View(await PaginatedList<Campaign>.CreateAsync(campaigns.AsNoTracking(), pageNumber ?? 1, pageSize));
+        }
+        #endregion 
+
+        #region Done
+        [Authorize(Roles = Constants.Constants.roleNameClient)]
+        [HttpGet]
+        public async Task<IActionResult> Done(int? pageNumber)
+        {
+            var user = await GetCurrentUserAsync();
+            var campaigns = from campaign in _context.Campaign
+                            from a in _context.ClientCampaign
+                            where a.UserId == user.Id && a.CampaignId == campaign.CampaignId
+                                && campaign.IsDone == true
+                            select campaign;
+
+            int pageSize = 6;
+            return View(await PaginatedList<Campaign>.CreateAsync(campaigns.AsNoTracking(), pageNumber ?? 1, pageSize));
+        }
+        #endregion 
+
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+    }
+}
