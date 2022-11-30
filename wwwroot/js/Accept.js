6685aa025f8239026f67f510945285e2397ba38e
daniel <danielalferia9@gmail.com>
2022-11-30 20:37:06 +0700

    bug fix align accept datatable

--- wwwroot/js/Accept.js
+++ wwwroot/js/Accept.js
@@ -22,31 +22,8 @@ var Acceptance = function () {
     self.ParticipantTable = function (data) {
         participantTable = $('#participantTable').DataTable({
             data: data,
-            order: [[1, 'asc'], [2, 'asc']],
-            scrollX: true,
-            scrollY: true,
-            scrollCollapse: true,
-            //dom: "<'#CustomHeaderDataTable'<'d-flex justify-content-between align-items-center'<'app-table-title'><'app-table-search d-flex'Bf>><'card-body p-0'tr>><'app-table-pagination d-flex justify-content-between p-1 'ilp>", // remove page length, filte
-            buttons: [
-                {
-                    text: '<table><tr><td><i class="ms-Icon ms-Icon--CalculatorAddition"></i></td><td class="buttonIcon">&nbsp;New</td></tr></table>',
-                    className: 'btn-sm btn-primary new-data-AdditionalLink',
-                    title: '',
-                    action: function (e, dt, node, config) {
-                        //self.resetFormAdditionalLink();
-                        //$("#modalAdditionalLink").modal("show");
-                    }
-                },
-                {
-                    text: '<table><tr><td><i class="ms-Icon ms-Icon--Refresh"></i></td><td class="buttonIcon">&nbsp;Refresh</td></tr></table>',
-                    title: '',
-                    className: 'btn-sm btn-primary new-data-AdditionalLink',
-                    title: 'Refresh Data',
-                    action: function (e, dt, node, config) {
-                        // self.AdditionalLinkTable.ajax.reload();
-                    }
-                }
-            ],
+            order: [1, 'asc'],
+            dom: "<'#CustomHeaderDataTable'<'d-flex justify-content-between align-items-center'<'app-table-title'><'app-table-search d-flex'Bf>><'card-body p-0'tr>><'app-table-pagination d-flex justify-content-between p-1 'ilp>",
             language: {
                 'search': '',
                 'searchPlaceholder': 'Quick Filter',
@@ -57,11 +34,25 @@ var Acceptance = function () {
                 { data: 'name', className: 'f-name', title: 'Name' },//1,
                 { data: 'gender', className: 'f-gender', title: 'Gender' },//2
                 { data: 'age', className: 'f-age', title: 'Age' },//3
-                { data: 'kota', className: 'f-kota', title: 'Kota' },//4
-                { data: 'provinsi', className: 'f-provinsi', title: 'Provinsi' },//5
-                { data: 'followers', className: 'f-followers', title: 'Followers' },//6
-                { data: 'er', className: 'f-er', title: 'Engagement Rate' },//7
-                { data: 'linkInstagram', className: 'f-linkInstagram', title: 'Instagram Url' },//8
+                {
+                    data: 'kota', className: 'f-kotaProvinsi', title: 'Address',
+                    render: function (data, type, row) {
+                        return data + ', ' + row['provinsi'];
+                    },
+                },//4
+                { data: 'followers', className: 'f-followers', title: 'Followers' },//5
+                {
+                    data: 'er', className: 'f-er', title: 'Engagement Rate',
+                    render: function (data, type, row, meta) {
+                        return data + ' %';
+                    }
+                },//6
+                {
+                    data: 'linkInstagram', className: 'f-linkInstagram', title: 'Instagram',
+                    render: function (data, type, row, meta) {
+                        return '<center><a id="linkInstagram' + meta.row + '" href="' + data + '" target="_blank">link</a></center>';
+                    }
+                },//7
                 {
                     data: 'isAccepted', className: 'f-isAccepted text-center', title: 'Is Accepted', visible: false , render: function (data, type, row, meta) {
                         if (data) {
@@ -71,7 +62,7 @@ var Acceptance = function () {
                             return '<center><input type="checkbox" class="form-check-input" id="isActiveChk_' + meta.row + '" disabled/></center>';
                         }
                     }
-                },//9
+                },//8
             ],
             orderCellsTop: true,
             initComplete: function (settings, json) {
