c2436a7f5d2f1e97634be23aac91d5ff205b12e9
daniel <danielalferia9@gmail.com>
2022-12-15 21:21:59 +0700

    Bug fix update profile creator. DoB

--- wwwroot/js/DatePicker.js
+++ wwwroot/js/DatePicker.js
@@ -1,3 +1,10 @@
 ﻿$(document).ready(function () {
     $("#datepicker").datepicker();
+    var initDob = document.getElementById("InitDob").value;
+    $("#datepicker").val(initDob);
+});
+
+$('#datepicker').on("change", function () {
+    var changeDob = document.getElementById("datepicker").value;
+    $("#hiddenDob").val(changeDob);
 });
