c64426b64810a5fc4adaa7421a4a0c2df9c4ce5b
daniel <danielalferia9@gmail.com>
2023-02-22 21:27:31 +0700

    fix datepicker

--- wwwroot/js/DatePicker.js
+++ wwwroot/js/DatePicker.js
@@ -1,10 +1,12 @@
 ﻿$(document).ready(function () {
-    $("#datepicker").datepicker();
+    $('#datepicker').datepicker({
+        onSelect: function (date) {
+            $('#datepicker').val(date);
+        },
+    }).change(function () {
+        var changeDob = document.getElementById("datepicker").value;
+        $("#hiddenDob").val(changeDob);
+    });
     var initDob = document.getElementById("InitDob").value;
     $("#datepicker").val(initDob);
-});
-
-$('#datepicker').on("change", function () {
-    var changeDob = document.getElementById("datepicker").value;
-    $("#hiddenDob").val(changeDob);
 });
